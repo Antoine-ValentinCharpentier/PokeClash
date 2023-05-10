fd40af5a3911fda7bdac2fbb9e434fa24463a296
Antoine-ValentinCharpentier <antoinevalentin.charpentier@gmail.com>
2023-05-10 22:21:44 +0200

    :bug: come back to auth page with token

--- client/src/pages/Auth/SignIn/SignInPage.tsx
+++ client/src/pages/Auth/SignIn/SignInPage.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
+
+import AuthContext from '../../../contexts/AuthContext';
+
 import SplitPage from '../../../components/Layout/AuthLayout';
 import SignInForm from './components/SignInForm'
 
 import '../../../assets/style/components/ui/Pokeball/Pokeball.css'
 
-
 interface SignInPageProps {}
 
 const SignInPage: React.FC<SignInPageProps> = () => {
+  const { tokens } = useContext(AuthContext)
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (tokens.accessToken) {
+      navigate('/');
+    }
+  }, [tokens, navigate]);
+
   return (
     <SplitPage form={<SignInForm/>}/>
   );
--- client/src/pages/Auth/SignUp/SignUpPage.tsx
+++ client/src/pages/Auth/SignUp/SignUpPage.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
+
+import AuthContext from '../../../contexts/AuthContext';
+
 import SplitPage from '../../../components/Layout/AuthLayout';
 import SignUpForm from './components/SignUpForm'
 
 import '../../../assets/style/components/ui/Pokeball/Pokeball.css'
 
-
 interface SignUpPageProps {}
 
 const SignUpPage: React.FC<SignUpPageProps> = () => {
+  const { tokens } = useContext(AuthContext)
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (tokens.accessToken) {
+      navigate('/');
+    }
+  }, [tokens, navigate]);
+
   return (
     <SplitPage form={<SignUpForm/>}/>
   );
